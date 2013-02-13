c35c973d4a78eafdb36a8d02499e4982bd71f7be
Clara Bayarri <nayami.ruka@gmail.com>
2013-02-13 19:44:04 +0100

    bug fix

--- src/main/java/com/example/service/ProblemServiceImpl.java
+++ src/main/java/com/example/service/ProblemServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,15 +9,19 @@ import com.example.model.TaskInfo;
 @Service
 public class ProblemServiceImpl implements ProblemService {
 
-	@Autowired
 	private RestTemplate template;
 	
 	public void setTemplate(RestTemplate template) {
 		this.template = template;
 	}
 	
+	private RestTemplate getTemplate() {
+		if (this.template == null) this.template = new RestTemplate();
+		return template;
+	}
+	
 	public Problem getProblem() {
-		TaskInfo task = template.getForObject("http://gentle-gorge-9660.herokuapp.com/API/task", TaskInfo.class);
+		TaskInfo task = getTemplate().getForObject("http://gentle-gorge-9660.herokuapp.com/API/task", TaskInfo.class);
 		return new Problem(task);
 	}
 	
